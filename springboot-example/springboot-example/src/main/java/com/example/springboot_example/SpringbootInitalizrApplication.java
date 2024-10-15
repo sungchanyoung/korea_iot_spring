51e387fb3abd68cc90e0bf5b8fec96c977b0fdf4
SungChanYoung <zkzk7290@naver.com>
2024-10-15 20:10:46 +0900

    20241015

---
+++ springboot-example/springboot-example/src/main/java/com/example/springboot_example/SpringbootInitalizrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitalizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInitalizrApplication.class, args);
+	}
+
+}
---
+++ springboot-example/springboot-example/src/test/java/com/example/springboot_example/SpringbootInitalizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInitalizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
